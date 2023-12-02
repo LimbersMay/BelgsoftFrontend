477934b05cf304eb5a243d5ff896f85ea3aad6c5
LimbersMay <limbermay1@gmail.com>
2023-12-02 12:25:54 -0600

    (components Field) Componente field creado para reusar los input

---
+++ src/components/Field.tsx
@@ -0,0 +1,19 @@
+import {HTMLInputTypeAttribute} from "react";
+
+interface FieldProps {
+    type: HTMLInputTypeAttribute;
+    name: string;
+    fieldName?: string;
+}
+
+export const Field = ({ type, name, fieldName }: FieldProps ) => {
+
+    const firstNameUpperCase = name.charAt(0).toUpperCase() + name.slice(1);
+
+    return (
+        <div className="flex flex-col gap-2">
+            <label htmlFor={name} className="font-bold text-white">{ fieldName ?? firstNameUpperCase }</label>
+            <input type={type} placeholder={ fieldName ?? firstNameUpperCase } name={name} className="w-96 rounded-md p-2"/>
+        </div>
+    )
+}
