1d5a497acd949cca989c1fabff7a9a4d52b186fe
LimbersMay <limbermay1@gmail.com>
2023-12-02 12:20:08 -0600

    (auth/routes AuthRouter) Se renderizó el componente de Register al acceder a la ruta de auth/register

--- src/auth/routes/AuthRouter.tsx
+++ src/auth/routes/AuthRouter.tsx
@@ -1,11 +1,11 @@
 import {Navigate, Route, Routes} from "react-router-dom";
-import {Login} from "../pages";
+import {Login, Register} from "../pages";
 
 export const AuthRouter = () => {
     return (
         <Routes>
             <Route path="/login" element={<Login />}/>
-            <Route path="/register" element={<h1>Register</h1>}/>
+            <Route path="/register" element={<Register />}/>
 
             <Route path="/*" element={<Navigate to="/auth/login" />}/>
         </Routes>
