041979e1b5052d42da423f3a5ff49772a6546a99
LimbersMay <limbermay1@gmail.com>
2023-12-05 13:38:10 -0600

    (hooks useAuthStore) hook de autenticación del usuario creado

---
+++ src/hooks/useAuthStore.ts
@@ -0,0 +1,93 @@
+import {checkingCredentials, login, logout, selectAuth, useAppDispatch, useAppSelector} from "../store";
+import {belgsoftApi} from "../api";
+
+
+interface CreatingUserProps {
+    name: string;
+    email: string;
+    password: string;
+}
+
+export const useAuthStore = () => {
+
+    const { uid, displayName, role, email, userType } = useAppSelector(selectAuth);
+
+    const dispatch = useAppDispatch();
+
+    const startCreatingUser = async ({name, email, password}: CreatingUserProps) => {
+        dispatch(checkingCredentials());
+
+        // Async call to create user
+        try {
+             await belgsoftApi.post('/auth/register', {
+                name,
+                email,
+                password
+             });
+
+             const response = await belgsoftApi.post('/auth/login', {
+                email,
+                password
+             });
+
+             const { id, name: username, email: userEmail, role, userType } = response.data;
+
+             dispatch(login({
+                 uid: id,
+                 displayName: username,
+                 email: userEmail,
+                 role,
+                 userType,
+             }));
+
+        } catch (error) {
+            dispatch(logout(null));
+        }
+    }
+
+    const startLogin = async (email: string, password: string) => {
+        dispatch(checkingCredentials());
+
+        try {
+            const response = await belgsoftApi.post('/auth/login', {
+                email,
+                password
+            }, {
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            });
+
+            const { user, token } = response.data;
+
+            // Save token in local storage
+            localStorage.setItem('token', token);
+
+            console.log(user);
+
+            dispatch(login({
+                uid: user.id,
+                displayName: user.name,
+                email: user.email,
+                role: user.role,
+                userType: user.userType,
+            }));
+        } catch (error) {
+            dispatch(logout(null));
+        }
+
+    }
+
+    return {
+        // properties
+        uid,
+        displayName,
+        role,
+        email,
+        userType,
+
+        // methods
+        startCreatingUser,
+        startLogin
+    }
+}
