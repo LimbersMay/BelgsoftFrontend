626f86b92540984345f8350aa7af3c50b764c9b2
LimbersMay <limbermay1@gmail.com>
2023-12-05 09:03:38 -0600

    (auth/pages Register.tsx) Formulario refactorizado con Formik

--- src/auth/pages/Register.tsx
+++ src/auth/pages/Register.tsx
@@ -1,35 +1,95 @@
-import {AuthLayout} from "../layouts/AuthLayout.tsx";
-import {Field} from "../../components/Field.tsx";
+import {AuthLayout} from "../layouts";
+import {ErrorMessage, Form, Formik, FormikHelpers} from "formik";
+import {Field} from "../../components";
+
+import * as Yup from 'yup';
+
+interface FormValues {
+    username: string;
+    email: string;
+    password: string;
+    repeatPassword: string;
+}
+
+const initialValues: FormValues = {
+    username: '',
+    email: '',
+    password: '',
+    repeatPassword: ''
+}
 
 export const Register = () => {
+
+    const onSubmit = (values: FormValues, {setSubmitting}: FormikHelpers<FormValues>) => {
+        setTimeout(() => {
+            console.log(values);
+            setSubmitting(false);
+        }, 4000);
+    }
+
     return (
-        <AuthLayout>
-            {/* Main container */}
-            <div className="flex justify-center">
-                {/* Form container */}
-                <div className="mt-10 mb-4 flex w-fit flex-col gap-5 rounded bg-neutral-800 p-4 px-8 pt-6 pb-8 shadow-md">
-                    <h1 className= "text-3xl font-bold text-white">
-                        Register
-                    </h1>
+        <AuthLayout title="Register">
+            <Formik
+                initialValues={{ ...initialValues }}
+                onSubmit={onSubmit}
+                validationSchema={Yup.object({
+                    username: Yup.string()
+                        .min(6, 'Must be 6 characters or more')
+                        .required('Required'),
+                    email: Yup.string()
+                        .email('Invalid email address')
+                        .required('Required'),
+                    password: Yup.string()
+                        .min(6, 'Must be 6 characters or more')
+                        .required('Required'),
+                    repeatPassword: Yup.string()
+                        .oneOf([Yup.ref('password')], 'Passwords must match')
+                        .required('Required'),
+                })}
+            >
+                {
+                    ({}) => (
+                        <Form className="flex flex-col gap-5">
+                            <Field
+                                type="text"
+                                name="username"
+                            />
 
-                    <form className="flex flex-col gap-5">
+                            <ErrorMessage name={"username"} component="div" className="text-red-500"/>
 
-                        <Field type={"text"} name={"username"} />
+                            <Field
+                                type="email"
+                                name="email"
+                            />
 
-                        <Field type={"email"} name={"email"} />
+                            <ErrorMessage name={"email"} component="div" className="text-red-500"/>
 
-                        <Field type={"password"} name={"password"} />
+                            <Field
+                                type="password"
+                                name="password"
+                            />
 
-                        <button
-                            type="submit"
-                            className="w-96 bg-neutral-700 hover:bg-neutral-600 text-white font-bold py-2 px-4 rounded"
-                        >
-                            Register
-                        </button>
-                    </form>
-                </div>
+                            <ErrorMessage name={"password"} component="div" className="text-red-500"/>
 
-            </div>
+                            <Field
+                                type="password"
+                                name="repeatPassword"
+                                fieldName="Repeat password"
+                            />
+
+                            <ErrorMessage name={"repeatPassword"} component="div" className="text-red-500"/>
+
+                            <button
+                                type="submit"
+                                className="w-96 bg-neutral-700 hover:bg-neutral-600 text-white font-bold py-2 px-4 rounded disabled:opacity-50 disabled:pointer-events-none"
+                            >
+                                Register
+                            </button>
+
+                        </Form>
+                    )
+                }
+            </Formik>
         </AuthLayout>
     )
 }
