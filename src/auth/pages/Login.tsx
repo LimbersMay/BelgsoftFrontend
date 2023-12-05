84a035394fa6442adb82d57957e28e4e57ccd06d
LimbersMay <limbermay1@gmail.com>
2023-12-05 09:01:34 -0600

    (auth/pages Login.tsx) Se refactorizó el formulario y se usó formik

--- src/auth/pages/Login.tsx
+++ src/auth/pages/Login.tsx
@@ -1,32 +1,74 @@
-import {AuthLayout} from "../layouts/AuthLayout.tsx";
-import {Field} from "../../components/Field.tsx";
+import {ErrorMessage, Form, Formik, FormikHelpers} from "formik";
+import * as Yup from 'yup';
+import {AuthLayout} from "../layouts";
+import {Field} from "../../components";
+
+interface FormValues {
+    email: string;
+    password: string;
+}
+
+const initialValues: FormValues = {
+    email: '',
+    password: ''
+}
 
 export const Login = () => {
+
+    const onSubmit = (values: FormValues, {setSubmitting}: FormikHelpers<FormValues>) => {
+        setTimeout(() => {
+            console.log(values);
+            setSubmitting(false);
+        }, 4000);
+    }
+
     return (
-        <AuthLayout>
-            {/* Main container */}
-            <div className="flex justify-center">
-                {/* Form container */}
-                <div className="mt-10 mb-4 flex w-fit flex-col gap-5 rounded bg-neutral-800 p-4 px-8 pt-6 pb-8 shadow-md">
-                    <h1 className= "text-3xl font-bold text-white">
-                        Login
-                    </h1>
+        <AuthLayout title={"Login"}>
+            <Formik
+                initialValues={{...initialValues}}
+                onSubmit={onSubmit}
+                validationSchema={Yup.object({
+                    email: Yup.string()
+                        .email('Invalid email address')
+                        .required('Required'),
+                    password: Yup.string()
+                        .min(6, 'Must be 6 characters or more')
+                        .required('Required'),
 
-                    <form className="flex flex-col gap-5">
+                })}
+            >
+                {
+                    ({
+                         isSubmitting,
+                     }) => (
+                        <Form className="flex flex-col gap-5">
 
-                        <Field type={"email"} name={"email"} />
+                            <Field
+                                type="email"
+                                name="email"
+                            />
 
-                        <Field type={"password"} name={"password"} />
+                            <ErrorMessage name={"email"} component="div" className="text-red-500"/>
 
-                        <button
-                            type="submit"
-                            className="w-96 bg-neutral-700 hover:bg-neutral-600 text-white font-bold py-2 px-4 rounded"
-                        >
-                            Login
-                        </button>
-                    </form>
-                </div>
-            </div>
+                            <Field
+                                type="password"
+                                name="password"
+                            />
+
+                            <ErrorMessage name={"password"} component="div" className="text-red-500"/>
+
+                            <button
+                                type="submit"
+                                className="w-96 bg-neutral-700 hover:bg-neutral-600 text-white font-bold py-2 px-4 rounded disabled:opacity-50 disabled:pointer-events-none"
+                                disabled={isSubmitting}
+                            >
+                                Login
+                            </button>
+                        </Form>
+                    )
+                }
+            </Formik>
+
         </AuthLayout>
     )
 }
