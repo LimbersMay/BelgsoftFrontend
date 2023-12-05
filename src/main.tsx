3241c728accb48f8338c5f7491cca48a687c5f6b
LimbersMay <limbermay1@gmail.com>
2023-12-05 09:30:55 -0600

    (main) Se llamó al interceptor de Axios en el archivo main

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import {BrowserRouter} from "react-router-dom";
+import {AxiosInterceptor} from "./interceptors";
+
+AxiosInterceptor();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
