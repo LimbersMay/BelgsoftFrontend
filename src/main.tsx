a71b1d36a8f405a279b352b54d337d3bf69f70e8
LimbersMay <limbermay1@gmail.com>
2023-12-05 13:40:09 -0600

    (main.tsx) Provider del store agregado

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,17 @@ import App from './App.tsx'
 import './index.css'
 import {BrowserRouter} from "react-router-dom";
 import {AxiosInterceptor} from "./interceptors";
+import {Provider} from "react-redux";
+import {store} from "./store";
 
 AxiosInterceptor();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+      <Provider store={store}>
+          <BrowserRouter>
+              <App />
+          </BrowserRouter>
+      </Provider>
   </React.StrictMode>,
 )
