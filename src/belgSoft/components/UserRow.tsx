34af927adf8f6875cfd4fe31ad36dae3f7571c1b
LimbersMay <limbermay1@gmail.com>
2023-12-06 11:13:23 -0600

    (belgsoft/components UserRow.tsx)  Se recibió directamente el objeto de User y se desestructuró para usarse en las columnas de la tabla

--- src/belgSoft/components/UserRow.tsx
+++ src/belgSoft/components/UserRow.tsx
@@ -1,11 +1,22 @@
-import {User} from "../admin/interfaces/User.ts";
+import {User} from "../admin";
+import {useUiStore, useUsersStore} from "../../hooks";
 
-export const UserRow = ({ID, name, email, role, plan, status}: User) => {
+export const UserRow = (user: User) => {
+
+    const { setActiveUser  } = useUsersStore();
+    const { showUpdatingUserModal } = useUiStore();
+
+    const { ID, username, email, role, plan, status } = user;
+
+    const handleEdit = () => {
+        setActiveUser(user);
+        showUpdatingUserModal();
+    }
 
     return (
         <tr>
             <td className="px-4 py-2 border">{ID}</td>
-            <td className="px-4 py-2 border">{name}</td>
+            <td className="px-4 py-2 border">{username}</td>
             <td className="px-4 py-2 border">
                 <a href="mailto:lim@google.com">
                     {email}
@@ -19,7 +30,10 @@ export const UserRow = ({ID, name, email, role, plan, status}: User) => {
                 </span>
             </td>
             <td className="px-4 py-2 border text-left">
-                <button className="px-4 py-2 mr-2 w-20 text-white bg-green-500 rounded hover:bg-green-600">
+                <button
+                    className="px-4 py-2 mr-2 w-20 text-white bg-green-500 rounded hover:bg-green-600"
+                    onClick={handleEdit}
+                >
                     Edit
                 </button>
                 <button className="px-4 py-2 text-white w-20 bg-red-500 rounded hover:bg-red-600">
