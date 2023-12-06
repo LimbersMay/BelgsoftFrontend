2b48edbd0fbaa29b9dbabf735352f0d8655d3067
LimbersMay <limbermay1@gmail.com>
2023-12-06 11:11:12 -0600

    (store store.ts) Slices de ui, users, role y usersState incluidos

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,15 @@
-import {Action, configureStore, ThunkAction} from "@reduxjs/toolkit";
-import {authSlice} from "./auth";
+import {configureStore} from "@reduxjs/toolkit";
+import {authSlice, usersSlice, uiSlice} from "./";
+import {roleSlice} from "./belgsoft/admin/authorization/RoleSlice.ts";
+import {userStateSlice} from "./belgsoft/admin/authorization/UserStateSlice.ts";
 
 export const store = configureStore({
     reducer: {
-        auth: authSlice.reducer
+        auth: authSlice.reducer,
+        users: usersSlice.reducer,
+        ui: uiSlice.reducer,
+        role: roleSlice.reducer,
+        userState: userStateSlice.reducer
     }
 })
 
@@ -11,10 +17,3 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
-
-export type AppThunk<ReturnType = void> = ThunkAction<
-    ReturnType,
-    RootState,
-    unknown,
-    Action
->
