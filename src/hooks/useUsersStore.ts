144e3711eda9b3779c86184facd5f86aa05de869
LimbersMay <limbermay1@gmail.com>
2023-12-06 11:07:39 -0600

    (hooks useUsersStore.ts) Se mapearon los atributos de roleId, statusId y planId para los usuarios

--- src/hooks/useUsersStore.ts
+++ src/hooks/useUsersStore.ts
@@ -12,21 +12,24 @@ export const useUsersStore = () => {
         const response = await belgsoftApi.get('/users');
         const users = response.data;
 
-        const mappedUsers: User[] = users.map(({ userId, name, email, role, userState, userType }: any) => {
+        const mappedUsers: User[] = users.map(({ userId, name, email, role, roleId, userState, userStateId, userType, userTypeId }: any) => {
             return {
                 ID: userId,
                 username: name,
                 email,
                 role: role,
+                roleId,
+                planId: userTypeId,
                 plan: userType,
                 status: userState,
+                statusId: userStateId,
             }
         });
 
         dispatch(setUsers(mappedUsers));
     }
 
-    const setActiveUser = (user: User) => {
+    const setActiveUser = (user: User | null) => {
         dispatch(onSetActiveUser(user));
     }
 
