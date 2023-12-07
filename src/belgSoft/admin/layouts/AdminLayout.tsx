57e46bbf75717b486a59d86865fc8f07e2241573
LimbersMay <limbermay1@gmail.com>
2023-12-06 22:58:12 -0600

    Add hooks for role and user state loading

--- src/belgSoft/admin/layouts/AdminLayout.tsx
+++ src/belgSoft/admin/layouts/AdminLayout.tsx
@@ -1,6 +1,8 @@
 import {BaseLayout} from "../../../layouts";
 import {ReactElement, useEffect} from "react";
 import {useUsersStore} from "../../../hooks/useUsersStore.ts";
+import {useRoleStore} from "../../../hooks/useRoleStore.ts";
+import {useUserStateStore} from "../../../hooks/useUserStateStore.ts";
 
 export const AdminLayout = ({ children }: { children: ReactElement }) => {
     const navLinks = [
@@ -12,15 +14,23 @@ export const AdminLayout = ({ children }: { children: ReactElement }) => {
             name: "Roles",
             path: "/roles"
         },
+        {
+            name: "User States",
+            path: "/user-states"
+        }
     ]
 
     // start loading the following data:
     // 1. Users who were created by the current user
     const { startLoadingUsers } = useUsersStore();
+    const { startLoadingRoles } = useRoleStore();
+    const { startLoadingUserStates } = useUserStateStore();
 
     useEffect(() => {
         (async () => {
             await startLoadingUsers();
+            await startLoadingRoles();
+            await startLoadingUserStates();
         })()
     }, []);
 
