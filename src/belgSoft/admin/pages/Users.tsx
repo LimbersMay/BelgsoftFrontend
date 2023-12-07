d2aa62314243353693d71d757c04b5a4d547cf79
LimbersMay <limbermay1@gmail.com>
2023-12-06 18:22:12 -0600

    (admin/pages Users.tsx) View de la tabla de usuarios creado

---
+++ src/belgSoft/admin/pages/Users.tsx
@@ -0,0 +1,47 @@
+import {AdminLayout} from "../layouts";
+import {CrudTable} from "../../components/CrudTable.tsx";
+import {UserRow} from "../../components/UserRow.tsx";
+import {CreateUserModal} from "../components";
+import {useUiStore, useUsersStore} from "../../../hooks";
+
+export const Users = () => {
+
+    const headers = [
+        'ID',
+        'Name',
+        'Email',
+        'Role',
+        'Plan',
+        'Status',
+        'Actions'
+    ]
+
+    const { users } = useUsersStore();
+    const { showCreatingUserModal } = useUiStore();
+
+    return (
+        <AdminLayout>
+            <div className="p-10">
+                <h1 className="text-4xl font-bold text-neutral-900">
+                    Users
+                </h1>
+
+                <button className="px-4 py-2 mt-4 text-white bg-green-500 rounded hover:bg-green-600" onClick={showCreatingUserModal}>
+                    Create User
+                </button>
+
+                {/* Table */}
+                <CrudTable headers={headers}>
+                    {
+                        users.map((user) => (
+                            <UserRow key={user.Id} {...user} />
+                        ))
+                    }
+                </CrudTable>
+
+
+                <CreateUserModal />
+            </div>
+        </AdminLayout>
+    )
+}
