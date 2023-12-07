f39d385e5f48322ae895a392988dc2b71c5b30d5
LimbersMay <limbermay1@gmail.com>
2023-12-07 15:00:44 -0600

    Refactor tablesSlice to include new actions and update state

--- src/store/belgsoft/admin/tablesSlice.ts
+++ src/store/belgsoft/admin/tablesSlice.ts
@@ -1,19 +1,51 @@
-import {createSlice} from '@reduxjs/toolkit';
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 import {RootState} from "../../store.ts";
+import {Table} from "../../../belgSoft/interfaces/table.interface.ts";
 
-export const productsSlice = createSlice({
+interface initialStateProps {
+    tables: Table[]
+    activeTable: Table | null
+}
+
+const initialState: initialStateProps = {
+    tables: [],
+    activeTable: null
+}
+
+export const tablesSlice = createSlice({
     name: 'products',
-    initialState: {
-        counter: 10
-    },
+    initialState: initialState,
     reducers: {
-        increment: (state, /* action */) => {
-            state.counter += 1;
+        setTables: (state, action: PayloadAction<Table[]>) => {
+            state.tables = action.payload;
         },
+        setTable: (state, action: PayloadAction<Table>) => {
+            state.tables.push(action.payload);
+        },
+        onSetActiveTable: (state, action: PayloadAction<Table | null>) => {
+            state.activeTable = action.payload;
+        },
+        updateTable: (state, action: PayloadAction<Partial<Table>>) => {
+            const index = state.tables.findIndex(table => table.id === action.payload.id);
+
+            state.tables[index] = {
+                ...state.tables[index],
+                ...action.payload
+            }
+        },
+        deleteTable: (state, action: PayloadAction<string>) => {
+            state.tables = state.tables.filter(table => table.id !== action.payload);
+        }
     }
 });
 
 
 // Action creators are generated for each case reducer function
-export const {increment} = productsSlice.actions;
-export const selectProducts = (state: RootState) => state.products;
+export const {
+    setTables,
+    setTable,
+    onSetActiveTable,
+    updateTable,
+    deleteTable
+} = tablesSlice.actions;
+export const selectTable = (state: RootState) => state.products;
