f6080de6f74260f3792bd40f93bd59217ac02ada
LimbersMay <limbermay1@gmail.com>
2023-12-07 15:01:24 -0600

    Add Product interface

---
+++ src/belgSoft/admin/interfaces/Product.ts
@@ -0,0 +1,12 @@
+
+export interface Product {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    image: string;
+    category: string;
+    quantity: number;
+    created_at: string;
+    updated_at: string;
+}
