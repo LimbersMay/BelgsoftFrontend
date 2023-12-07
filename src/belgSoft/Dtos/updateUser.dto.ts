ba3e24b87b975f106f30918dc99386d0762f0233
LimbersMay <limbermay1@gmail.com>
2023-12-07 00:08:29 -0600

    Refactor UserDTO to include additional properties

--- src/belgSoft/Dtos/updateUser.dto.ts
+++ src/belgSoft/Dtos/updateUser.dto.ts
@@ -1,15 +1,21 @@
 import {CreateUserModalProps} from "../admin";
 
-export interface UserDTO extends Omit<CreateUserModalProps, "username"> {
+export interface UserDTO extends Omit<CreateUserModalProps, "username" | "statusId" | "roleId" | "planId"> {
     name: CreateUserModalProps["username"];
+    userStateId: CreateUserModalProps["statusId"];
+    roleId: CreateUserModalProps["roleId"];
+    userTypeId: CreateUserModalProps["planId"];
 }
 
 export const fromFormValuesToUserDTO = (values: Partial<CreateUserModalProps>): Partial<UserDTO> => {
-    const {username, ...rest} = values;
+    const { username, statusId, roleId, planId, ...rest } = values;
 
     return {
         ...rest,
-        name: username
+        name: username,
+        userStateId: statusId,
+        roleId: roleId,
+        userTypeId: planId,
     };
-
 }
+
