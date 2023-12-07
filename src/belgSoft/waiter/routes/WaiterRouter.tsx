679840fe51dbc7fb27080acbc038951a37ee93ba
LimbersMay <limbermay1@gmail.com>
2023-12-07 15:05:28 -0600

    Add route for Tables page

--- src/belgSoft/waiter/routes/WaiterRouter.tsx
+++ src/belgSoft/waiter/routes/WaiterRouter.tsx
@@ -1,10 +1,11 @@
 import {Navigate, Route, Routes} from "react-router-dom";
-import {Home} from "../pages";
+import {Home, Tables} from "../pages";
 
 export const WaiterRouter = () => {
     return (
         <Routes>
             <Route path="/" element={<Home />}/>
+            <Route path="/tables" element={<Tables />}/>
 
             <Route path="/*" element={<Navigate to="/" />}/>
         </Routes>
