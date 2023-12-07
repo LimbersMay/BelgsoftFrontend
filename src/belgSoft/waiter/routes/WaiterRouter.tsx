17eab6de4563a1d2313ed2a2c42bc956002ee61a
LimbersMay <limbermay1@gmail.com>
2023-12-06 23:24:22 -0600

    Add routes for WaiterRouter

--- src/belgSoft/waiter/routes/WaiterRouter.tsx
+++ src/belgSoft/waiter/routes/WaiterRouter.tsx
@@ -1,1 +1,12 @@
+import {Navigate, Route, Routes} from "react-router-dom";
+import {Home} from "../pages";
 
+export const WaiterRouter = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<Home />}/>
+
+            <Route path="/*" element={<Navigate to="/" />}/>
+        </Routes>
+    )
+}
