35d9d2114a00bcd4f140a8a11b82520367a2c8ea
LimbersMay <limbermay1@gmail.com>
2023-12-07 16:01:18 -0600

    Add Areas page to WaiterRouter

--- src/belgSoft/waiter/routes/WaiterRouter.tsx
+++ src/belgSoft/waiter/routes/WaiterRouter.tsx
@@ -1,11 +1,13 @@
 import {Navigate, Route, Routes} from "react-router-dom";
 import {Home, Tables} from "../pages";
+import {Areas} from "../pages/Areas.tsx";
 
 export const WaiterRouter = () => {
     return (
         <Routes>
             <Route path="/" element={<Home />}/>
             <Route path="/tables" element={<Tables />}/>
+            <Route path="/areas" element={<Areas />}/>
 
             <Route path="/*" element={<Navigate to="/" />}/>
         </Routes>
