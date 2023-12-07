c35bccd6ab78c9b5413e0b09c9ce54c50437d394
LimbersMay <limbermay1@gmail.com>
2023-12-07 00:09:20 -0600

    Refactor RoleRow component to use custom hooks

--- src/belgSoft/admin/components/RoleRow.tsx
+++ src/belgSoft/admin/components/RoleRow.tsx
@@ -1,15 +1,28 @@
 import {Role} from "../interfaces/Role.ts";
+import {useRoleStore, useUiStore} from "../../../hooks";
 
-export const RoleRow = ({ id, name, value }: Role) => {
+export const RoleRow = (role: Role) => {
+
+    const { showRoleModal } = useUiStore();
+    const { setActiveRole } = useRoleStore();
+
+    const handleEdit = () => {
+        showRoleModal();
+
+        setActiveRole(role);
+    }
+
+    const { id, roleName, value } = role;
+
     return (
         <tr>
             <td className="px-4 py-2 border">{id}</td>
-            <td className="px-4 py-2 border">{name}</td>
+            <td className="px-4 py-2 border">{roleName}</td>
             <td className="px-4 py-2 border">{value}</td>
             <td className="px-4 py-2 border text-left">
                 <button
                     className="px-4 py-2 mr-2 w-20 text-white bg-green-500 rounded hover:bg-green-600"
-                    onClick={() => console.log('edit')}
+                    onClick={handleEdit}
                 >
                     Edit
                 </button>
