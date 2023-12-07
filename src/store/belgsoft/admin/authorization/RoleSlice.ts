b8f7e4a3df4410218e60cfb9e7b9970ff7346878
LimbersMay <limbermay1@gmail.com>
2023-12-07 12:05:04 -0600

    Add createRole and updateRole actions to RoleSlice

--- src/store/belgsoft/admin/authorization/RoleSlice.ts
+++ src/store/belgsoft/admin/authorization/RoleSlice.ts
@@ -21,11 +21,22 @@ export const roleSlice = createSlice({
         },
         onSetActiveRole: (state, action: PayloadAction<Role | null>) => {
             state.activeRole = action.payload;
+        },
+        createRole: (state, action: PayloadAction<Role>) => {
+            state.roles.push(action.payload);
+        },
+        updateRole: (state, action: PayloadAction<Partial<Role>>) => {
+            const roleIndex = state.roles.findIndex(role => role.id === action.payload.id);
+
+            state.roles[roleIndex] = {
+                ...state.roles[roleIndex],
+                ...action.payload
+            }
         }
     }
 });
 
 
 // Action creators are generated for each case reducer function
-export const { setRoles, onSetActiveRole} = roleSlice.actions;
+export const { setRoles, onSetActiveRole, createRole, updateRole} = roleSlice.actions;
 export const selectRole = (state: RootState) => state.role;
