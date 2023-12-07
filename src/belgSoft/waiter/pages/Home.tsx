51064d24f6d982175ebb2018f300f62cb3c4e281
LimbersMay <limbermay1@gmail.com>
2023-12-07 12:05:31 -0600

    Add image and title to home page

--- src/belgSoft/waiter/pages/Home.tsx
+++ src/belgSoft/waiter/pages/Home.tsx
@@ -1,9 +1,17 @@
 import {HomeLayout} from "../layouts/HomeLayout.tsx";
+import tiaKaua from "../../../assets/tiaKaua.jpeg";
 
 export const Home = () => {
     return (
         <HomeLayout>
-            <div>Waiter</div>
+            <div className="flex p-10 flex-col items-center justify-center">
+
+                <img src={tiaKaua} alt="tiaKaua" className="w-96"/>
+
+                <h1 className={"text-4xl text-neutral-900"}>
+                    Sistema de administración de la tia de kaua
+                </h1>
+            </div>
         </HomeLayout>
     )
 }
