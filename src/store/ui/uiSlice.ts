fbc0475c9d20c38f3ac3941d104a8d74a3e9929a
LimbersMay <limbermay1@gmail.com>
2023-12-07 15:00:02 -0600

    Add table modal functionality

--- src/store/ui/uiSlice.ts
+++ src/store/ui/uiSlice.ts
@@ -4,11 +4,13 @@ import {RootState} from "../store";
 export interface uiState {
     isShowingUserModal: boolean,
     isShowingRoleModal: boolean,
+    isShowingTableModal: boolean,
 }
 
 const initialState: uiState = {
     isShowingUserModal: false,
     isShowingRoleModal: false,
+    isShowingTableModal: false,
 }
 
 export const uiSlice = createSlice({
@@ -27,10 +29,23 @@ export const uiSlice = createSlice({
         onHideRoleModal: (state) => {
             state.isShowingRoleModal = false;
         },
+        onShowTableModal: (state) => {
+            state.isShowingTableModal = true;
+        },
+        onHideTableModal: (state) => {
+            state.isShowingTableModal = false;
+        },
     }
 });
 
 
 // Action creators are generated for each case reducer function
-export const { onShowUserModal, onHideUserModal, onShowRoleModal, onHideRoleModal } = uiSlice.actions;
+export const {
+    onShowUserModal,
+    onHideUserModal,
+    onShowRoleModal,
+    onHideRoleModal ,
+    onShowTableModal,
+    onHideTableModal
+} = uiSlice.actions;
 export const selectUi = (state: RootState) => state.ui;
