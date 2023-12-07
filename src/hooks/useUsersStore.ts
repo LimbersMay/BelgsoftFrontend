6b99e19a277c13d16773a05e898a7f93b10bf63f
LimbersMay <limbermay1@gmail.com>
2023-12-07 00:08:07 -0600

    Add user creation and update functionality

--- src/hooks/useUsersStore.ts
+++ src/hooks/useUsersStore.ts
@@ -1,20 +1,25 @@
-import {onSetActiveUser, useAppDispatch, useAppSelector} from "../store";
+import {createUser, onSetActiveUser, updateUser, useAppDispatch, useAppSelector} from "../store";
 import {belgsoftApi} from "../api";
 import {selectUsers, setUsers} from "../store";
-import {User} from "../belgSoft/admin";
+import {CreateUserModalProps, User} from "../belgSoft/admin";
+import {fromFormValuesToUserDTO} from "../belgSoft/Dtos/updateUser.dto.ts";
+import {useAuthStore} from "./useAuthStore.ts";
 
 export const useUsersStore = () => {
 
     const dispatch = useAppDispatch();
+
     const { users, activeUser } = useAppSelector(selectUsers);
+    const { branchId: authenticatedUserBranchId } = useAuthStore();
 
     const startLoadingUsers = async () => {
         const response = await belgsoftApi.get('/users');
         const users = response.data;
 
-        const mappedUsers: User[] = users.map(({ userId, name, email, role, roleId, userState, userStateId, userType, userTypeId }: any) => {
+        const mappedUsers: User[] = users.map(({ userId, branchId, name, email, role, roleId, userState, userStateId, userType, userTypeId }: any) => {
             return {
-                ID: userId,
+                branchId,
+                Id: userId,
                 username: name,
                 email,
                 role: role,
@@ -29,6 +34,38 @@ export const useUsersStore = () => {
         dispatch(setUsers(mappedUsers));
     }
 
+    const startUpdatingUser = async (user: Partial<CreateUserModalProps>) => {
+
+        const dataToSend = fromFormValuesToUserDTO(user);
+        const response = await belgsoftApi.put(`/users/${user.Id}`, {...dataToSend });
+
+        // Assign the new status to the user
+        user.status = response.data.userState;
+        user.role = response.data.role;
+
+        dispatch(updateUser(user));
+    }
+
+    const startCreatingUser = async (user: CreateUserModalProps) => {
+        const dataToSend = fromFormValuesToUserDTO(user);
+
+        const response = await belgsoftApi.post(`/users`, {
+            ...dataToSend,
+            branchId: authenticatedUserBranchId
+        });
+
+        const { branchId, userId, role, userType, userState} = response.data;
+
+        dispatch(createUser({
+            ...user,
+            branchId,
+            Id: userId,
+            role,
+            plan: userType,
+            status: userState
+        }));
+    }
+
     const setActiveUser = (user: User | null) => {
         dispatch(onSetActiveUser(user));
     }
@@ -40,6 +77,8 @@ export const useUsersStore = () => {
 
         // Methods
         startLoadingUsers,
+        startUpdatingUser,
+        startCreatingUser,
         setActiveUser
     }
 }
