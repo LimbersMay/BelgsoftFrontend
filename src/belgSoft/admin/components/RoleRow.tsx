ebe083266a2ca93996274a46339226925269e955
LimbersMay <limbermay1@gmail.com>
2023-12-07 15:02:23 -0600

    Add disabled state to Edit and Delete buttons in RoleRow component

--- src/belgSoft/admin/components/RoleRow.tsx
+++ src/belgSoft/admin/components/RoleRow.tsx
@@ -1,5 +1,6 @@
 import {Role} from "../interfaces/Role.ts";
 import {useRoleStore, useUiStore} from "../../../hooks";
+import {RBACTypes} from "../types/RBAC-types.ts";
 
 export const RoleRow = (role: Role) => {
 
@@ -21,12 +22,16 @@ export const RoleRow = (role: Role) => {
             <td className="px-4 py-2 border">{value}</td>
             <td className="px-4 py-2 border text-left">
                 <button
-                    className="px-4 py-2 mr-2 w-20 text-white bg-green-500 rounded hover:bg-green-600"
+                    className="px-4 py-2 mr-2 w-20 text-white bg-green-600 rounded hover:bg-green-700 disabled:bg-green-400"
                     onClick={handleEdit}
+                    disabled={roleName === RBACTypes.ADMIN}
                 >
                     Edit
                 </button>
-                <button className="px-4 py-2 text-white w-20 bg-red-500 rounded hover:bg-red-600">
+                <button
+                    className="px-4 py-2 text-white w-20 bg-red-500 rounded hover:bg-red-600 disabled:bg-red-400"
+                    disabled={roleName === RBACTypes.ADMIN}
+                >
                     Delete
                 </button>
             </td>
