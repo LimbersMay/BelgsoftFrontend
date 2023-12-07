116fc7abab82e4291b183f6595a8e0580e2ad5fe
LimbersMay <limbermay1@gmail.com>
2023-12-07 16:01:26 -0600

    Add Areas page to Waiter app

---
+++ src/belgSoft/waiter/pages/Areas.tsx
@@ -0,0 +1,9 @@
+import {HomeLayout} from "../layouts/HomeLayout.tsx";
+
+export const Areas = () => {
+    return (
+        <HomeLayout>
+            <h1>Areas</h1>
+        </HomeLayout>
+    )
+}
