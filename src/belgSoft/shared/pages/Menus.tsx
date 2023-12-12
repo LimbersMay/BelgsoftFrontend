62021f7ffef57b556ff4482e8ddce3e55a84f640
LimbersMay <limbermay1@gmail.com>
2023-12-12 15:53:28 -0600

    Add Menus page component

---
+++ src/belgSoft/shared/pages/Menus.tsx
@@ -0,0 +1,50 @@
+import {useAuthStore, useMenuStore, useUiStore} from "../../../hooks";
+import {CrudTable} from "../components";
+import {RBACTypes} from "../../admin/types/RBAC-types.ts";
+import {MenuRow} from "../components/MenuRow.tsx";
+import {MenuModal} from "../components/MenuModal.tsx";
+
+
+export const Menus = () => {
+
+    const headers = [
+        'ID',
+        'Name',
+        'Description',
+        'Price',
+        'Available',
+        'Category',
+    ]
+
+    const { menus } = useMenuStore();
+    const { showMenuModal } = useUiStore();
+    const { role } = useAuthStore();
+
+    return (
+        <div className="p-10">
+            <h1 className="text-4xl font-bold text-neutral-900">
+                Menus
+            </h1>
+
+            {
+                (role === RBACTypes.ADMIN) &&
+                (
+                    <button className="px-4 py-2 mt-4 text-white bg-green-500 rounded hover:bg-green-600" onClick={showMenuModal}>
+                        Create Menu
+                    </button>
+                )
+            }
+
+            {/* Table */}
+            <CrudTable headers={headers}>
+                {
+                    menus.map((menu) => (
+                        <MenuRow key={menu.menuId} {...menu} />
+                    ))
+                }
+            </CrudTable>
+
+            <MenuModal />
+        </div>
+    )
+}
