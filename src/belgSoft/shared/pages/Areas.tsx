7c8935fb3947dbbc2a84a0d12c86f705ef95b9f8
LimbersMay <limbermay1@gmail.com>
2023-12-12 15:18:04 -0600

    Refactor Areas page component

--- src/belgSoft/shared/pages/Areas.tsx
+++ src/belgSoft/shared/pages/Areas.tsx
@@ -1,9 +1,7 @@
-import {HomeLayout} from "../layouts/HomeLayout.tsx";
-import {CrudTable} from "../../components";
-import {useAreaStore} from "../../../hooks/useAreaStore.ts";
-import {AreaRow} from "../../shared/components/AreaRow.tsx";
-import {useUiStore} from "../../../hooks";
-import {AreaModal} from "../../shared/components/AreaModal.tsx";
+import {useAreaStore, useAuthStore, useUiStore} from "../../../hooks";
+import {AreaModal, AreaRow, CrudTable} from "../components";
+import {RBACTypes} from "../../admin/types/RBAC-types.ts";
+
 
 export const Areas = () => {
 
@@ -15,17 +13,22 @@ export const Areas = () => {
 
     const { areas } = useAreaStore();
     const { showAreaModal } = useUiStore();
+    const { role } = useAuthStore();
 
     return (
-        <HomeLayout>
             <div className="p-10">
                 <h1 className="text-4xl font-bold text-neutral-900">
                     Areas
                 </h1>
 
-                <button className="px-4 py-2 mt-4 text-white bg-green-500 rounded hover:bg-green-600" onClick={showAreaModal}>
-                    Create Area
-                </button>
+                {
+                    (role === RBACTypes.ADMIN) &&
+                    (
+                        <button className="px-4 py-2 mt-4 text-white bg-green-500 rounded hover:bg-green-600" onClick={showAreaModal}>
+                            Create Area
+                        </button>
+                    )
+                }
 
                 {/* Table */}
                 <CrudTable headers={headers}>
@@ -38,6 +41,5 @@ export const Areas = () => {
 
                 <AreaModal />
             </div>
-        </HomeLayout>
     )
 }
