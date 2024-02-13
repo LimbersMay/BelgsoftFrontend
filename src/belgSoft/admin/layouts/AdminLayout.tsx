d6591a831f030575039433c74a1726a871dee27e
LimbersMay <limbermay1@gmail.com>
2024-02-12 21:44:07 -0600

    Add hooks for loading areas, menus, orders, and tables***

--- src/belgSoft/admin/layouts/AdminLayout.tsx
+++ src/belgSoft/admin/layouts/AdminLayout.tsx
@@ -1,6 +1,13 @@
 import {BaseLayout} from "../../../layouts";
 import {ReactElement, useEffect} from "react";
-import {useRoleStore, useUsersStore, useUserStateStore} from "../../../hooks";
+import {
+    useAreaStore,
+    useMenuStore,
+    useOrderStore,
+    useRoleStore, useTablesStore,
+    useUsersStore,
+    useUserStateStore
+} from "../../../hooks";
 export const AdminLayout = ({ children }: { children: ReactElement }) => {
     const navLinks = [
         {
@@ -12,22 +19,42 @@ export const AdminLayout = ({ children }: { children: ReactElement }) => {
             path: "/roles"
         },
         {
-            name: "User States",
-            path: "/user-states"
-        }
+            name: "Areas",
+            path: "/areas"
+        },
+        {
+            name: "Tables",
+            path: "/tables"
+        },
+        {
+            name: "Menus",
+            path: "/menus"
+        },
+        {
+            name: "Orders",
+            path: "/orders"
+        },
     ]
 
     // start loading the following data:
     // 1. Users who were created by the current user
     const { startLoadingUsers } = useUsersStore();
     const { startLoadingRoles } = useRoleStore();
+    const { startLoadingMenus } = useMenuStore();
     const { startLoadingUserStates } = useUserStateStore();
+    const { startLoadingAreas } = useAreaStore();
+    const { startLoadingOrders } = useOrderStore();
+    const { startLoadingTables } = useTablesStore();
 
     useEffect(() => {
         (async () => {
             await startLoadingUsers();
             await startLoadingRoles();
             await startLoadingUserStates();
+            await startLoadingAreas();
+            await startLoadingMenus();
+            await startLoadingOrders();
+            await startLoadingTables();
         })()
     }, []);
 
