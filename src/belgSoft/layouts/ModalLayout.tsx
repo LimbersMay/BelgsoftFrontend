129d6611a2a216ca6171f0f244381f19e3d65d8f
LimbersMay <limbermay1@gmail.com>
2024-02-12 21:45:22 -0600

    Refactor ModalLayout component to support multiple children and add 'Select' mode

--- src/belgSoft/layouts/ModalLayout.tsx
+++ src/belgSoft/layouts/ModalLayout.tsx
@@ -1,10 +1,10 @@
 import {ReactElement} from "react";
 
 interface ModalProps {
-    children?: ReactElement;
+    children?: ReactElement | ReactElement[];
     show: boolean;
     onClose: () => void;
-    mode: 'Create' | 'Update';
+    mode: 'Create' | 'Update' | 'Select';
 }
 
 export const ModalLayout = ({children, mode, show, onClose}: ModalProps) => {
@@ -12,21 +12,25 @@ export const ModalLayout = ({children, mode, show, onClose}: ModalProps) => {
         <>
             {
                 show && (
-                    <div className="fixed inset-0 flex items-center justify-center">
-                        <div className="fixed inset-0 bg-black opacity-50" onClick={onClose}></div>
-                        <div className="relative z-50 bg-white p-4 max-w-md mx-auto rounded shadow-lg flex flex-col gap-2">
-                            {
-                                <h2 className="text-2xl font-bold text-green-800">
-                                    <span className="text-neutral-950 mr-2">{mode}</span>
-                                    <span className="text-green-800">Record</span>
-                                </h2>
-                            }
+                    <>
+                        <div className="fixed inset-0 flex items-center justify-center">
+                            <div className="fixed inset-0 bg-black opacity-50" onClick={onClose}></div> {/* Black opacity background */}
 
-                            <hr/>
-
-                            {children}
+                            <div className="flex">
+                                <div className="bg-black opacity-50" onClick={onClose}></div>
+                                <div className="relative z-50 bg-white p-4 rounded shadow-lg flex flex-col gap-2">
+                                    <h2 className="text-2xl font-bold text-green-800">
+                                        <span className="text-neutral-950 mr-2">{mode}</span>
+                                        <span className="text-green-800">Record</span>
+                                    </h2>
+                                    <hr/>
+                                    {
+                                        children
+                                    }
+                                </div>
+                            </div>
                         </div>
-                    </div>
+                    </>
                 )
             }
         </>
