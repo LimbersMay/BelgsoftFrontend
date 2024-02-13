e14014bc47e763e17d84e5e22fd3788edd32e9bf
LimbersMay <limbermay1@gmail.com>
2024-02-12 21:45:39 -0600

    Update ModalField component to include disabled and value props

--- src/belgSoft/shared/components/ModalField.tsx
+++ src/belgSoft/shared/components/ModalField.tsx
@@ -1,14 +1,16 @@
 import {Field} from "formik";
 import {HTMLInputTypeAttribute} from "react";
-import {firstCapitalLetter} from "../../helpers/firstCapitalLetter.ts";
+import {firstCapitalLetter} from "../../../helpers/firstCapitalLetter.ts";
 
 interface ModalFieldProps {
     name: string;
     fieldName?: string;
     type: HTMLInputTypeAttribute;
+    disabled?: boolean;
+    value?: any;
 }
 
-export const ModalField = ({ name, fieldName, type }: ModalFieldProps) => {
+export const ModalField = ({ name, fieldName, type, disabled = false}: ModalFieldProps) => {
 
     return (
         <div className="flex flex-col">
@@ -16,8 +18,10 @@ export const ModalField = ({ name, fieldName, type }: ModalFieldProps) => {
             <Field
                 type={ type }
                 name={ name }
+                id={ name }
                 placeholder={ fieldName ?? firstCapitalLetter(name) }
-                className="w-96 rounded-md p-2 border-2"
+                className="w-96 rounded-md p-2 border-2 disabled:bg-gray-200"
+                disabled={ disabled }
             />
         </div>
     )
