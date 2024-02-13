f71ca7f62c2d1a26e7fbd597b2a395bbdad382d9
LimbersMay <limbermay1@gmail.com>
2024-02-12 21:44:00 -0600

    Add new interfaces for Area, Menu, Order, Product, Role, and UserState

--- src/belgSoft/admin/interfaces/index.ts
+++ src/belgSoft/admin/interfaces/index.ts
@@ -1,1 +1,7 @@
+export * from './Area';
+export * from './Menu';
+export * from './Order';
+export * from './Product';
+export * from './Role';
 export * from './User';
+export * from './UserState';
